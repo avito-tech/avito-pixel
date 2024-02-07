a98d68894e72fadefa10751bf8a8a3f007a393cc
Nikita Bobrov <nabobrov@avito.ru>
2024-02-07 19:31:42 +0300

    feat: add TLS options for clickhouse

--- lib/storage/storage.go
+++ lib/storage/storage.go
@@ -1,7 +1,12 @@
 package storage
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
+	"path"
+	"strings"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
@@ -34,6 +39,44 @@ func NewClickhouse(
 }
 
 func newClient(conf config.Clickhouse) (driver.Conn, error) {
+
+	var (
+		tlsConf      *tls.Config
+		clientCAs    *x509.CertPool
+		certificates []tls.Certificate
+	)
+
+	if len(strings.TrimSpace(conf.CaCertFile)) > 0 {
+		caCert, err := os.ReadFile(path.Join(conf.CaCertFile))
+		if err != nil {
+			panic(err)
+		}
+		caCertPool := x509.NewCertPool()
+		ok := caCertPool.AppendCertsFromPEM(caCert)
+		if !ok {
+			panic("failed to append from pem")
+		}
+		clientCAs = caCertPool
+	}
+
+	if len(string(conf.Cert)) > 0 && len(string(conf.CertKey)) > 0 {
+		cert, err := tls.LoadX509KeyPair(conf.Cert, conf.CertKey)
+		if err != nil {
+			panic(err)
+		}
+		certificates = []tls.Certificate{cert}
+	}
+
+	if conf.SkipVerify != nil || len(certificates) > 0 || clientCAs != nil {
+		tlsConf = &tls.Config{
+			Certificates: certificates,
+			ClientCAs:    clientCAs,
+		}
+		if conf.SkipVerify != nil && *conf.SkipVerify {
+			tlsConf.InsecureSkipVerify = true
+		}
+	}
+
 	opts := clickhouse.Options{
 		Addr: []string{fmt.Sprintf("%s:%d", conf.Host, conf.Port)},
 		Auth: clickhouse.Auth{
@@ -44,6 +87,7 @@ func newClient(conf config.Clickhouse) (driver.Conn, error) {
 		MaxOpenConns:    conf.MaxOpenConns,
 		MaxIdleConns:    conf.MaxIdleConns,
 		ConnMaxLifetime: conf.ConnLifetime,
+		TLS:             tlsConf,
 		Settings: map[string]interface{}{
 			"max_threads": conf.MaxThreads,
 		},
